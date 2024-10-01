802ecf81adbb8ace911067297385b09bad2f47a7
tgoddessana <twicegoddessana1229@gmail.com>
2024-10-01 21:02:03 +0900

    fix: fix get_client_ip return value

--- django_logbox/middlewares.py
+++ django_logbox/middlewares.py
@@ -99,4 +99,9 @@ class LogboxMiddleware:
 
     @staticmethod
     def _get_client_ip(request: HttpRequest):
-        return request.META.get("REMOTE_ADDR", None)
+        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(",")[0]
+        else:
+            ip = request.META.get("REMOTE_ADDR")
+        return ip
